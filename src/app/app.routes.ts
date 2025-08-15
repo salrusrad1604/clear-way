db4b4071e2f896a383916872e2b8a2d6c106eaed
Ruslan Salakhov <salrusrad@yandex.ru>
2025-08-15 18:32:41 +0300

    основные правки, доброска файлов

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DocumentView } from './components/document-view/document-view';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'document/:id', component: DocumentView },
+  { path: '**', redirectTo: '/document/1' },
+];
---
+++ src/mock/mock-document.ts
@@ -0,0 +1,25 @@
+export const documentMock = {
+  name: 'Название документа',
+  pages: [
+    {
+      number: 1,
+      imageUrl: 'assets/pages/1.png',
+    },
+    {
+      number: 2,
+      imageUrl: 'assets/pages/2.png',
+    },
+    {
+      number: 3,
+      imageUrl: 'assets/pages/3.png',
+    },
+    {
+      number: 4,
+      imageUrl: 'assets/pages/4.png',
+    },
+    {
+      number: 5,
+      imageUrl: 'assets/pages/5.png',
+    },
+  ],
+};
