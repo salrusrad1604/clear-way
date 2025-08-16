f36649beaced135f2741a0d8ae452ca658da4ff4
Ruslan Salakhov <salrusrad@yandex.ru>
2025-08-16 21:35:37 +0300

    fix import

--- src/app/components/anotation/annotation.ts
+++ src/app/components/anotation/annotation.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, HostBinding, HostListener, Input, Output, signal, WritableSignal } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Annotation } from '../../model/annotation-model';
 import { ButtonModule } from 'primeng/button';
 
